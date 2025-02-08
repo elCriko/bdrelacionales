a999c3937597c90615a3ac8f30151bfed794df6c
elCriko <isaac.moreno23@cetis107.edu.mx>
2025-02-07 18:18:57 -0700

    07/01/2025-Practica#2 Creacion de tabla alumnos

---
+++ Parcial 1/Practica2/alumnos.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-02-2025 a las 02:13:57
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cetis107`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `numero_control` varchar(20) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `genero` tinyint(1) NOT NULL,
+  `correo` varchar(80) NOT NULL,
+  `telefono` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `numero_control`, `fecha_nacimiento`, `genero`, `correo`, `telefono`) VALUES
+(1, 'Mateo', 'Ibarra Gastélum', '23325061070484', '2008-01-11', 1, 'mateo.ibar09@gmail.com', '6678619006'),
+(2, 'Oscar Ivan', 'Torres Burgos', '23325061070510', '2008-08-22', 1, 'ivan.torres@gmail.com', '6679913009'),
+(3, 'Emiliano', 'Ojeda Quintero', '23325061070702', '2008-07-22', 1, 'emiliano@gmail.com', '667450678'),
+(4, 'Hector Aaron', 'Ortiz Serrano', '23325060870750', '2008-08-23', 1, 'hector.ortiz23@cetis107.edu.mx', '6673583222'),
+(5, 'Uriel', 'Rodriguez Rojo', '23325061080828', '2008-02-11', 1, 'uriel@gmail.com', '6675006547');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
