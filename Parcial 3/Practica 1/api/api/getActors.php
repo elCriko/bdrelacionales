ab43b3a50fb2fbe9dec576f8cf4e4d62b2d238ee
elCriko <isaac.moreno23@cetis107.edu.mx>
2025-06-02 18:32:17 -0700

    02/06/2025 Practica 2- api

---
+++ Parcial 3/Practica 1/api/api/getActors.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+    $host =  '192.168.1.21';
+    $user = 'sakila_user';
+    $pass = 'sakila';
+    $dbname = 'sakila';
+   
+    
+    $conn = new mysqli($host, $user, $pass, $dbname);
+    
+
+    if($conn->connect_error){
+        http_response_code(500);
+        echo json_encode(['error' => 'Error de conexion' . $conn->connect_error]);
+        exit;
+    }
+
+    $sql = "SELECT * FROM actor";
+    $rsul = $conn->query($sql);
+    
+    if($resul && $resul->num_rows > 0){
+        while($row = result->mysql_fetch_assoc()){
+            $actors[] = row;
+
+        }
+
+    }
+    
+
+    $conn->close();
+
+    header("content-Type: application/json");
+    echo json_encode($actors);
+    
+
+    
+
+?>
