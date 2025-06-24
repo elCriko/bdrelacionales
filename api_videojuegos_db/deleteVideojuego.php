6f811150e854de94418fdf1d6e264cfafa1abce1
elCriko <isaac.moreno23@cetis107.edu.mx>
2025-06-23 18:24:33 -0700

    Update deleteVideojuego.php

--- api_videojuegos_db/deleteVideojuego.php
+++ api_videojuegos_db/deleteVideojuego.php
@@ -1,16 +1,16 @@
 <?php
-// Conexión a la base de datos
+
 $conn = new mysqli("localhost", "root", "", "videojuegos_db");
 
-// Recibir datos JSON del body
+
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Extraer el ID a eliminar
+
 $id = $data["id"];
 
-// Eliminar el registro
+
 $sql = "DELETE FROM videojuegos WHERE id = $id";
 
-// Enviar resultado
+
 echo json_encode(["success" => $conn->query($sql)]);
 ?>
