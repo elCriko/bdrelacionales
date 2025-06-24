0f91026141a941ac339df521dd75104d952e0930
elCriko <isaac.moreno23@cetis107.edu.mx>
2025-06-23 18:24:12 -0700

    updateVideojuego.php

--- api_videojuegos_db/updateVideojuego.php
+++ api_videojuegos_db/updateVideojuego.php
@@ -1,18 +1,18 @@
 <?php
-// Conexión a la base de datos
+
 $conn = new mysqli("localhost", "root", "", "videojuegos_db");
 
-// Recibir datos JSON del body
+
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Extraer valores
+
 $id = $data["id"];
 $nombre = $data["nombre"];
 $descripcion = $data["descripcion"];
 $fecha = $data["fecha_lanzamiento"];
 $plataforma = $data["plataforma"];
 
-// Actualizar el registro
+
 $sql = "UPDATE videojuegos SET 
         nombre = '$nombre',
         descripcion = '$descripcion',
@@ -20,6 +20,6 @@ $sql = "UPDATE videojuegos SET
         plataforma = '$plataforma'
         WHERE id = $id";
 
-// Enviar resultado
+
 echo json_encode(["success" => $conn->query($sql)]);
 ?>
