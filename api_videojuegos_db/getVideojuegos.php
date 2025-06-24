36331e5ecfc68d6c7757be8795524ea04f5e8f8e
elCriko <isaac.moreno23@cetis107.edu.mx>
2025-06-23 18:23:49 -0700

    getVideojuegos.php

--- api_videojuegos_db/getVideojuegos.php
+++ api_videojuegos_db/getVideojuegos.php
@@ -1,19 +1,19 @@
 <?php
-// Conexión a la base de datos
+
 $conn = new mysqli("localhost", "root", "", "videojuegos_db");
 
-// Consulta todos los registros
+
 $sql = "SELECT * FROM videojuegos";
 $result = $conn->query($sql);
 
-// Arreglo para almacenar los datos
+
 $datos = [];
 
 while ($row = $result->fetch_assoc()) {
     $datos[] = $row;
 }
 
-// Regresa los datos en formato JSON
+
 header('Content-Type: application/json');
 echo json_encode($datos);
 ?>
